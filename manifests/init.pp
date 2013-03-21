f1baab0a8fa4ccd3fea774807224fc668d9efdff
Will Farrington <wfarr@github.com>
2013-03-21 14:31:07 -0400

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+# Public: Install VMware Fusion.
+#         This does not include any license, and you must provide your own.
+#         By installing VMware Fusion, you accept its Terms of Service.
+#
+# Usage:
+#
+#   include vmware_fusion
+class vmware_fusion {
+  package { 'VMware Fusion':
+    source   => 'https://s3.amazonaws.com/boxen-downloads/vmware/VMware-Fusion-5.0.3-1040386-light.dmg',
+    provider => 'appdmg'
+  }
+}
---
+++ spec/classes/vmware_fusion_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'vmware_fusion' do
+  it do
+    should contain_package('VMware Fusion').with({
+      :source   => 'https://s3.amazonaws.com/boxen-downloads/vmware/VMware-Fusion-5.0.3-1040386-light.dmg',
+      :provider => 'appdmg'
+    })
+  end
+end
+
