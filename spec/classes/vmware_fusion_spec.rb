cf7a7dbc0420c04fab22a239ad067edd06daa333
David Goodlad <david@goodlad.net>
2014-09-10 22:37:33 -0700

    Test the -light condition based on version

--- spec/classes/vmware_fusion_spec.rb
+++ spec/classes/vmware_fusion_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe 'vmware_fusion' do
-  it do
-    should contain_package('VMware Fusion').with({
-      :source   => 'https://s3.amazonaws.com/boxen-downloads/vmware/VMware-Fusion-6.0.2-1398658-light.dmg',
-      :provider => 'appdmg'
-    })
+  context "VMWare Fusion < 7" do
+    let(:params) { { :version  => '6.0.2-1398658' } }
+    it do
+      should contain_package('VMware Fusion').with({
+        :source   => 'https://s3.amazonaws.com/boxen-downloads/vmware/VMware-Fusion-6.0.2-1398658-light.dmg',
+        :provider => 'appdmg'
+      })
+    end
+  end
+
+  context "VMWare Fusion 7" do
+    let(:params) { { :version  => '7.0.0-2103067' } }
+    it do
+      should contain_package('VMware Fusion').with({
+        :source   => 'https://s3.amazonaws.com/boxen-downloads/vmware/VMware-Fusion-7.0.0-2103067.dmg',
+        :provider => 'appdmg'
+      })
+    end
   end
 end
-
